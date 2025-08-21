a82c24eebb4ce1bfe7351eb09fcdf3a9e8218aae
Thanh Danh <danhdt90@propolife.co.jp>
2025-08-21 11:46:05 +0700

    add xdebug and repair docker

--- config/Dockerfile.php
+++ config/Dockerfile.php
@@ -7,4 +7,12 @@ RUN docker-php-ext-install mysqli pdo pdo_mysql
 RUN apt-get update && apt-get install -y \
     libzip-dev \
     zip \
-    && docker-php-ext-install zip
+    libxml2-dev \
+    libcurl4-openssl-dev \
+    libonig-dev \
+    libfreetype6-dev \
+    libjpeg62-turbo-dev \
+    libpng-dev \
+    && docker-php-ext-configure gd --with-freetype --with-jpeg \
+    && docker-php-ext-install zip xml curl mbstring ctype gd \
+    && docker-php-ext-enable zip xml curl mbstring ctype gd
